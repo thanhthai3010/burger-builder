125c81ba86f1916e929409602aa942946479ac18
ThaiNT <thai@emobi.biz>
2020-05-05 15:18:52 +0700

    Module 8 - Finish video 19

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -81,7 +81,11 @@ module.exports = function(webpackEnv) {
       },
       {
         loader: require.resolve('css-loader'),
-        options: cssOptions,
+        options: {
+          modules: {
+            localIdentName: '[name]__[local]__[hash:base64:5]'
+          }
+        },
       },
       {
         // Options for PostCSS as we reference these options twice
---
+++ src/components/BuildControls/BuildControls.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    <p>
+      Current Price: <strong>{props.price.toFixed(2)}</strong>
+    </p>
+    {controls.map((ctr) => (
+      <BuildControl
+        key={ctr.label}
+        label={ctr.label}
+        added={() => props.ingredientAdded(ctr.type)}
+        removed={() => props.ingredientRemoved(ctr.type)}
+        disabled={props.disabled[ctr.type]}
+      />
+    ))}
+    <button className={classes.OrderButton} disabled={!props.purchaseable}>
+      ORDER NOW
+    </button>
+  </div>
+);
+
+export default buildControls;
